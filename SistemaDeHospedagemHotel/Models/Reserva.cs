0be73ef69d759826ca5249dcebfef43393bc077b
Thiago <prattithiago5@gmail.com>
2024-05-27 16:51:09 -0300

    Desafio DIO 2: Sistema de Hospedagem de Hotel

---
+++ SistemaDeHospedagemHotel/Models/Reserva.cs
@@ -0,0 +1,54 @@
+namespace DesafioProjetoHospedagem.Models
+{
+    public class Reserva
+    {
+        public List<Pessoa> Hospedes { get; set; }
+        public Suite Suite { get; set; }
+        public int DiasReservados { get; set; }
+
+        public Reserva(){}
+
+        public Reserva(int diasReservados)
+        {
+            DiasReservados = diasReservados;
+        }
+
+        public void CadastrarHospedes(List<Pessoa> hospedes)
+        {
+            if (Suite.Capacidade >= hospedes.Count)
+            {
+                Hospedes = hospedes;
+            }
+            else
+            {
+                throw new Exception("a capacidade de hospedes foi ultrapassada");
+            }
+        }
+
+        public void CadastrarSuite(Suite suite)
+        {
+            Suite = suite;
+        }
+
+        public int ObterQuantidadeHospedes()
+        {
+            return Hospedes.Count;
+        }
+
+        public decimal CalcularValorDiaria()
+        {
+
+            decimal valor = DiasReservados * Suite.ValorDiaria;
+
+
+            if (DiasReservados >= 10)
+            {
+                decimal desconto = valor * 0.1M;
+
+                valor = valor - desconto;
+            }
+
+            return valor;
+        }
+    }
+}
--- SistemaDeHospedagemHotel/obj/Debug/net8.0/SistemaDeHospedagemHotel.AssemblyInfo.cs
+++ SistemaDeHospedagemHotel/obj/Debug/net8.0/SistemaDeHospedagemHotel.AssemblyInfo.cs
@@ -10,12 +10,12 @@
 using System;
 using System.Reflection;
 
-[assembly: System.Reflection.AssemblyCompanyAttribute("SistemaDeEstacionamento")]
+[assembly: System.Reflection.AssemblyCompanyAttribute("SistemaDeHospedagemHotel")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
 [assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c478f483e9607074f5cad8e224af8902f2446167")]
-[assembly: System.Reflection.AssemblyProductAttribute("SistemaDeEstacionamento")]
-[assembly: System.Reflection.AssemblyTitleAttribute("SistemaDeEstacionamento")]
+[assembly: System.Reflection.AssemblyProductAttribute("SistemaDeHospedagemHotel")]
+[assembly: System.Reflection.AssemblyTitleAttribute("SistemaDeHospedagemHotel")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
 
 // Gerado pela classe WriteCodeFragment do MSBuild.
