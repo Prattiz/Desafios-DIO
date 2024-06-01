e0bc339b531ea9a45fdbe68091ef99feb8f35726
Thiago <prattithiago5@gmail.com>
2024-06-01 11:04:43 -0300

    .

--- SistemaMoblie/obj/Debug/net8.0/SistemaMoblie.AssemblyInfo.cs
+++ SistemaMoblie/obj/Debug/net8.0/SistemaMoblie.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("SistemaMoblie")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+8e1da161bcc328a36b11cdb7fc852c32c8b8b462")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f9f5fd5b6a3d0c914152b1a63e7e6814dc6060c1")]
 [assembly: System.Reflection.AssemblyProductAttribute("SistemaMoblie")]
 [assembly: System.Reflection.AssemblyTitleAttribute("SistemaMoblie")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
